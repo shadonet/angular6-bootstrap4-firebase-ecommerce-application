84b43f49725ef6401c9e84309a9b5359db3a6108
shadonet <ingprs01@gmail.com>
2018-02-18 07:10:59 +0100

    Refactor project boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductComponent } from './product/product.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CategoryComponent } from './category/category.component';
+import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductComponent,
+    CheckoutComponent,
+    CategoryComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
